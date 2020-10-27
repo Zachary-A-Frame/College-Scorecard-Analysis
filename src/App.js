41343283884e734cfaf713934fc0346c2e70c9d6
Zach <Zacaframe@gmail.com>
2020-10-27 00:43:03 -0400

    Fixed React Router

--- src/App.js
+++ src/App.js
@@ -24,9 +24,9 @@ function App() {
       <Header subtitle={subtitle} />
       <Navbar />
       <Switch>
-        <Route path={`${process.env.PUBLIC_URL}/College-Scorecard-Analysis`} component={LandingPage} exact/>
+        <Route path={`/College-Scorecard-Analysis`} component={LandingPage} exact/>
         <Route
-          path={`${process.env.PUBLIC_URL}/`}
+          path={`/`}
           component={LandingPage}
           exact
         />
