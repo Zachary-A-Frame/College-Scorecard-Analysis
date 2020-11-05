ddd299067a50039b65c696f288678ff71e476519
Zach <Zacaframe@gmail.com>
2020-11-05 16:43:11 -0500

    Updates

---
+++ precache-manifest.615557d29679d137ead5c18956e3b543.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de99f8287fb87f8f0d2f14e0fdb59e9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9a306482b04f8df8f9c",
+    "url": "/static/css/main.69058deb.chunk.css"
+  },
+  {
+    "revision": "d202ada8b78de9cf6d94",
+    "url": "/static/js/2.3e86e204.chunk.js"
+  },
+  {
+    "revision": "ccb96163cb81453816364e1c2beb6525",
+    "url": "/static/js/2.3e86e204.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9a306482b04f8df8f9c",
+    "url": "/static/js/main.acd973aa.chunk.js"
+  },
+  {
+    "revision": "f50348748b29cf06a695",
+    "url": "/static/js/runtime-main.4470b636.js"
+  },
+  {
+    "revision": "0a3795f056a1a2f04ec0ddce49256574",
+    "url": "/static/media/confusion-matrix-2.0a3795f0.png"
+  },
+  {
+    "revision": "53e62934da00f4d2cb7121e52784503d",
+    "url": "/static/media/confusion-matrix.53e62934.png"
+  },
+  {
+    "revision": "bfb6473e533b7bdc83f2cc5fede605a3",
+    "url": "/static/media/earnings_tuition.bfb6473e.png"
+  },
+  {
+    "revision": "1fb1edbbf769d7345fca0e5685ffd3f1",
+    "url": "/static/media/elasticnet_results_debt_df.1fb1edbb.csv"
+  },
+  {
+    "revision": "7c7051da04f4913c87793f8db3ae6529",
+    "url": "/static/media/elasticnet_results_sorted_earnings_df.7c7051da.csv"
+  },
+  {
+    "revision": "119d3c9e0dec154063eaa54339624fc2",
+    "url": "/static/media/elasticnet_results_sorted_majors_df.119d3c9e.csv"
+  },
+  {
+    "revision": "ca12e36d7a229f0d770913897cfd7760",
+    "url": "/static/media/lasso_results_sorted_debt_df.ca12e36d.csv"
+  },
+  {
+    "revision": "dbe2ceb3a2fc33d2c7787447ef751458",
+    "url": "/static/media/lasso_results_sorted_earnings_df.dbe2ceb3.csv"
+  },
+  {
+    "revision": "ded5cfa1f49119001952adaa46c7fa9d",
+    "url": "/static/media/lasso_results_sorted_majors_df.ded5cfa1.csv"
+  },
+  {
+    "revision": "0d7bc8dddcb6aca38ccd5f12ddf28531",
+    "url": "/static/media/loading-animation.0d7bc8dd.gif"
+  },
+  {
+    "revision": "d8d479d57d050e32c1d77bb859d61ce2",
+    "url": "/static/media/ml-mapOnHomepage.d8d479d5.png"
+  },
+  {
+    "revision": "ecef6b089acd38ae80d387100bba1e97",
+    "url": "/static/media/random_forest_debt_summary_table.ecef6b08.csv"
+  },
+  {
+    "revision": "26c5320195160d97a8f810717a5a7abf",
+    "url": "/static/media/random_forest_major_earnings_summary_table.26c53201.csv"
+  },
+  {
+    "revision": "85810e62401f387c6537c14d2336dce4",
+    "url": "/static/media/random_forest_summary_college_major_tree.85810e62.png"
+  },
+  {
+    "revision": "8ddc8df4b8217d108834592f194c6db5",
+    "url": "/static/media/random_forest_summary_debt_tree.8ddc8df4.png"
+  },
+  {
+    "revision": "0001efa31f6b32d07ed81149cded05bd",
+    "url": "/static/media/random_forest_summary_earnings_tree.0001efa3.png"
+  },
+  {
+    "revision": "d4dc0fd92dbf0bb5f42ef91aec4ef8c7",
+    "url": "/static/media/random_forest_summary_forbes_best_tree.d4dc0fd9.png"
+  },
+  {
+    "revision": "f1f856e8379e1584e60a1c55937f3ef2",
+    "url": "/static/media/random_forest_summary_forbes_df.f1f856e8.csv"
+  }
+]);
